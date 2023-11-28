9c7a61a49b4a5943c67880f96c5c9deb64a3bcc1
borisloor06 <boris.loor06@gmail.com>
2023-11-27 19:48:02 -0500

    add stopwords

--- src/cleanDataFunctions/cleanData.py
+++ src/cleanDataFunctions/cleanData.py
@@ -1,4 +1,7 @@
 from functools import lru_cache
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+stop_words = set(stopwords.words('english'))
 
 def clean_reddit_data(dataframe, column):
     # Drop duplicates if necessary
@@ -9,6 +12,7 @@ def clean_reddit_data(dataframe, column):
 
     df[column] = df[column].apply(clean_text)
     df[column] = df[column].apply(clean_data)
+    df[column] = df[column].apply(delete_stopwords)
 
     return df
 
@@ -33,6 +37,23 @@ def clean_data(text):
         return text.encode("utf-8", "replace").decode("utf-8")
     except Exception as e:
         return "?"
+    
+@lru_cache()
+def delete_stopwords(text):
+    if not validar_str(text):
+        return text
+    # Usar un conjunto para las stopwords
+
+    # Tokenizar el texto de manera más eficiente
+    words = word_tokenize(text)
+
+    # Filtrar las stopwords
+    filtered_words = [word for word in words if word.lower() not in stop_words]
+
+    # Unir las palabras filtradas en un solo texto
+    filtered_text = ' '.join(filtered_words)
+
+    return filtered_text
 
 @lru_cache()
 def validar_str(text):
