139456209d3c78fa5041b49a2686c8905de67181
borisloor06 <boris.loor06@gmail.com>
2023-12-03 22:47:31 -0500

    only new subbreddits

--- src/saveDbDataFunctions/functions.py
+++ src/saveDbDataFunctions/functions.py
@@ -74,8 +74,8 @@ async def get_subreddit_posts(app, subreddit_name, start_date_str, comments_coll
 
                 posts.append(await fetch_posts_data(post, posts_collection_name, start_date, db))
 
-        await fetchData(subreddits_top)
-        await fetchData(subreddits_hot)
+        # await fetchData(subreddits_top)
+        # await fetchData(subreddits_hot)
         await fetchData(subreddits_new)
 
         all_posts = pd.DataFrame(posts, columns=post_columns)
