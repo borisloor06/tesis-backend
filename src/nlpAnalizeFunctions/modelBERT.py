d5aecab43b90ccaa5be253c01e7e611867a671a6
borisloor06 <boris.loor06@gmail.com>
2023-12-06 17:09:20 -0500

    fix truncation for tokenizer

--- src/nlpAnalizeFunctions/modelBERT.py
+++ src/nlpAnalizeFunctions/modelBERT.py
@@ -4,23 +4,25 @@ import torch
 import pandas as pd
 from transformers import pipeline
 
+
 class SentimentAnalyzer:
     def __init__(self, model_name="SamLowe/roberta-base-go_emotions"):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.sentiment_analysis_pipe = pipeline("sentiment-analysis", 
-                                                framework="pt", 
-                                                model=model_name,
-                                                tokenizer=model_name,
-                                                device=self.device,
-                                                )
-     
+        self.sentiment_analysis_pipe = pipeline(
+            "sentiment-analysis",
+            framework="pt",
+            model=model_name,
+            device=self.device,
+            truncation=True,
+            max_length=512,
+        )
+
     def getSentiment(self, dataframe, text_column):
         sentiments = dataframe[text_column].apply(self.getSentimentWithPipeline)
         print(sentiments.head(10))
         sentiments = pd.DataFrame(sentiments.tolist(), columns=["label", "score"])
         print(sentiments.head(10))
         return sentiments
-
 
     def getSentimentWithPipeline(self, text):
         results = self.sentiment_analysis_pipe(text)[0]
