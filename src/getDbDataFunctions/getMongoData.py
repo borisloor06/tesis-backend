1f5c24be0c6b579bf5676aa8bc8e7ed0429b9132
borisloor06 <boris.loor06@gmail.com>
2024-01-07 15:52:55 -0500

    eliminar hilos al obtener la data

--- src/getDbDataFunctions/getMongoData.py
+++ src/getDbDataFunctions/getMongoData.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import asyncio
-from concurrent.futures import ThreadPoolExecutor
 from src.cleanDataFunctions.cleanData import cleanData
 
 columns_used = [
@@ -83,55 +81,8 @@ async def getPost(db, posts_collection_name="reddit_posts"):
 
 
 async def getAnalisis(db, posts_collection_name="ChatGpt_analisis"):
-    with ThreadPoolExecutor() as executor:
-        result = await asyncio.to_thread(db[posts_collection_name].find, {}, {"_id": 0})
-    return result
-
-async def joinPostWithComments(
-    db, comments_collection_name="reddit_comments", posts_collection_name="reddit_posts"
-):
-    data = await getCommentsAndPostConcurrent(
-        db,
-        comments_collection_name,
-        posts_collection_name,
-    )
-    comments, posts = await data[0], await data[1]
-    comments = pd.DataFrame(comments)
-    posts = pd.DataFrame(posts)
-
-    comments = comments.add_prefix("comments_")
-    posts = posts.add_prefix("posts_")
-    result_df = pd.merge(
-        comments,
-        posts,
-        left_on="comments_subreddit_id",
-        right_on="posts_id",
-        how="inner",
-    )
-    return result_df
-
-
-async def getCommentsAndPostConcurrent(
-    db, comments_collection_name="reddit_comments", posts_collection_name="reddit_posts"
-):
-    with ThreadPoolExecutor() as executor:
-        comments_future = executor.submit(getComments, db, comments_collection_name)
-        posts_future = executor.submit(getPost, db, posts_collection_name)
-
-        comments = await asyncio.to_thread(comments_future.result)
-        posts = await asyncio.to_thread(posts_future.result)
-    return [comments, posts]
-
-async def getDataUnclean(db, query):
-    comments_collection = f'{query}_comments'
-    posts_collection = f'{query}_posts'
-    print(db)
-    comentarios = db[comments_collection].count_documents({})
-    posts = db[posts_collection].count_documents({})
-    print("------------------------COMENTARIOS-----------------------------")
-    print(comentarios, posts)
-    data = await joinPostWithComments(db, comments_collection, posts_collection)
-    return data
+    cursor = db[posts_collection_name].find({}, {"_id": 0}).batch_size(2000)
+    return list(cursor)
 
 async def getData(db, query):
     comments_collection = f'{query}_comments'
@@ -156,3 +107,21 @@ async def getData(db, query):
     data = data.drop(columns=["comments_created_date", "posts_created_date"], axis=1)
     data = cleanData(data)
     return data
+
+async def getCommentsAndPost(db, query):
+    comments_collection = f'{query}_comments'
+    posts_collection = f'{query}_posts'
+    comments = await getComments(db, comments_collection)
+    posts = await getPost(db, posts_collection)
+    comments = pd.DataFrame(comments)
+    posts = pd.DataFrame(posts)
+    comments = comments.add_prefix("comments_")
+    posts = posts.add_prefix("posts_")
+    result_df = pd.merge(
+        comments,
+        posts,
+        left_on="comments_subreddit_id",
+        right_on="posts_id",
+        how="inner",
+    )
+    return result_df
